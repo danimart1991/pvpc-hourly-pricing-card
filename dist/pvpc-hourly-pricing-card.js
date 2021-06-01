610c90b927b5d2a31595786f09fbc258c12c7629
jjprz <jj.p3rez@gmail.com>
2021-06-01 09:30:49 +0200

    Update getTariffPeriod()
    GetTariffPeriod() is updated to the new rate 2.0TD

--- dist/pvpc-hourly-pricing-card.js
+++ dist/pvpc-hourly-pricing-card.js
@@ -582,38 +582,62 @@ class PVPCHourlyPricingCard extends LitElement {
     return data;
   }
 
-  getTariffPeriod(tariff) {
-    let period;
-
-    switch (tariff) {
-      case 'normal':
-        period = 'normal';
-        break;
-      case 'discrimination':
-        const utcHours = new Date().getUTCHours();
-        if (utcHours >= 21 || utcHours < 11) {
-          period = 'valley';
-        } else {
-          period = 'peak';
-        }
-        break;
-      case 'electric_car':
-        const hours = new Date().getHours();
-        if (hours >= 1 && hours < 7) {
-          period = 'super-valley';
-        } else if (hours >= 13 && hours < 23) {
-          period = 'peak';
-        } else {
-          period = 'valley';
-        }
-        break;
-      default:
-        period = 'error';
-    }
-
-    return period;
+  getTariffPeriod() {
+      let period;
+      var now = new Date();
+      var hour = now.getHours();
+      var dayOfWeek = now.getDay();
+      if (hour < 8) {
+          period = "valley";
+      } else if (dayOfWeek == 6 || dayOfWeek == 0 || this.isFestive()) {
+          period = "valley";
+      } else if ((hour >= 10 && hour < 14) || (hour >= 18 && hour < 22)) {
+          period = "peak";
+      } else {
+          period = "normal"
+      }
+      return period;
   }
+  
+  isFestive() {
+      let festives = ["01-01", "01-05", "12-10", "01-11", "06-12", "08-12", "25-12"];
 
+      let ss = ["01-04-2018", "21-04-2019", "12-04-2020", "04-04-2021", "17-04-2022", "09-04-2023", "31-03-2024", "20-04-2025", "05-04-2026", "28-03-2027",
+        "16-04-2028", "01-04-2029", "21-04-2030", "13-04-2031", "28-03-2032", "17-04-2033", "09-04-2034", "25-03-2035", "13-04-2036", "05-04-2037",
+        "25-04-2038", "10-04-2039", "01-04-2040", "21-04-2041", "06-04-2042", "29-03-2043", "17-04-2044", "09-04-2045", "25-03-2046", "14-04-2047",
+        "05-04-2048", "18-04-2049", "10-04-2050", "02-04-2051", "21-04-2052", "06-04-2053", "29-03-2054", "18-04-2055", "02-04-2056", "22-04-2057"];
+
+      if (festives.indexOf(this.formatDate(new Date())) > -1) {
+          return true;
+      } else if (ss.indexOf(this.formatFullDate(new Date())) > -1) {
+          return true;
+      } else {
+          return false;
+      }
+  }
+  
+  formatDate(datetime) {
+      let day = datetime.getDate();
+      let month = datetime.getMonth() + 1;
+  
+      if (day < 10) day = "0" + day;
+      if (month < 10) month = "0" + month;
+  
+      return day + "-" + month;
+  }
+  
+  formatFullDate(datetime) {
+      datetime.setDate(datetime.getDate() - 5)
+      let day = datetime.getDate();
+      let month = datetime.getMonth() + 1;
+      let year = datetime.getFullYear();
+  
+      if (day < 10) day = "0" + day;
+      if (month < 10) month = "0" + month;
+  
+      return day + "-" + month + "-" + year;
+  }
+  
   getDateString(datetime) {
     return new Date(datetime).toLocaleDateString(this.lang, {
       day: '2-digit',
